859d1d58584dc54d3bb335681ba3721d93fed7d1
Stephanie Chang <ischang@ucdavis.edu>
2022-07-03 00:35:52 -0400

    Update move_books.sh

--- move_books.sh
+++ move_books.sh
@@ -1,8 +1,11 @@
-#cron job in cron scheduler 
-
+# for: without readarr and/or without calibre integration in readarr
+# for manual cronning
 
 find /volume1/downloads/[[FOLDER]]/books -name '*.epub' -exec mv {} /volume1/docker/calibre/upload/ \;
 
 find /volume1/downloads/[[FOLDER]]/books -name '*.pdf' -exec mv {} /volume1/docker/calibre/upload/ \;
 
 find /volume1/downloads/[[FOLDER]]/books -depth -type d -empty -exec rmdir {} \;
+
+
+# manual upload w readarr by adding to Wanted manually 
